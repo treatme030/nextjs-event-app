de0d861515e41bbd32de431e680e57f0d12b68d7
treatme030 <treatme8192@gmail.com>
2022-07-16 20:21:49 +0900

    initialize

---
+++ pages/events/[eventId].js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EventDetailPage = () => {
+  return (
+    <div>
+      <h1>Event Detail</h1>
+    </div>
+  );
+};
+
+export default EventDetailPage;
+<h1>Event Detail</h1>;
---
+++ pages/events/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllEventsPage = () => {
+  return (
+    <div>
+      <h1>All Events</h1>
+    </div>
+  );
+};
+
+export default AllEventsPage;
